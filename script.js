b3fdc3e27a439e5977bd26c7d431b720ffb02073
Lev <lev.bereza@gmail.com>
2025-09-22 19:49:04 +0300

    feat: add second task

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const imageLink = 'https://aws-travel-guide.s3.eu-west-1.amazonaws.com/default_image_size/6042a289a49a2_%D0%9A%D0%B8%D1%97%D0%B2.jpeg';
+
 const el9 = document.getElementById('el9');
 const el10 = document.querySelector('#el10');
 el9.addEventListener('click', () => {
@@ -19,5 +21,42 @@ el10.addEventListener('click', () => {
     } else {
         el10.classList.remove('blue-background');
         el10.classList.add('green-background');
+    }
+});
+
+const container = document.querySelector('.image-link');
+let img = document.getElementById('image');
+
+document.getElementById('addBtn').addEventListener('click', () => {
+    if (!img) {
+        const newImg = document.createElement('img');
+        newImg.id = 'image';
+        newImg.src = imageLink;
+        newImg.style.width = '200px';
+        container.appendChild(newImg);
+        img = newImg;
+    }
+});
+
+document.getElementById('increaseBtn').addEventListener('click', () => {
+    if (img) {
+        let currentWidth = img.width;
+        img.style.width = currentWidth + 20 + 'px';
+    }
+});
+
+document.getElementById('decreaseBtn').addEventListener('click', () => {
+    if (img) {
+        let currentWidth = img.width;
+        if (currentWidth > 40) {
+            img.style.width = currentWidth - 20 + 'px';
+        }
+    }
+});
+
+document.getElementById('removeBtn').addEventListener('click', () => {
+    if (img) {
+        img.remove();
+        img = null;
     }
 });
